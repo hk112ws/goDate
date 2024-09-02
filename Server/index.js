9dac14f74b2b913df14d2647167d74757a8bbf15
hk112ws <manashkalita360@gmail.com>
2024-09-02 17:59:09 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -12,7 +12,6 @@ app.use(cors(
 		
   origin: 'https://go-date.vercel.app', // Replace with your actual Vercel URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 	}
 ));
